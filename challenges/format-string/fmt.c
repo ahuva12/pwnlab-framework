f47411e08daf3e2161cc585482e1a2799298a846
ahuva12 <ahuvaeshit@gmail.com>
2026-05-02 22:10:51 +0300

    format string bug

--- challenges/format-string/fmt.c
+++ challenges/format-string/fmt.c
@@ -11,15 +11,19 @@ void safe() {
 
 int main() {
     char input[100];
-	void (*func_ptr)() = safe;
-
+	void (*func_ptr)() = safe;	
+    void *addr_func_ptr = &func_ptr;
+//  printf("Hint: %p\n", &func_ptr);
+//  puts("Something in your input might be interpreted, not printed.");
     printf("What is your name?\n");
+	
     fgets(input, sizeof(input), stdin);
 
-    printf(input); 
+    printf(input); 	
 
     func_ptr();	
 
     return 0;
 }
+
 
