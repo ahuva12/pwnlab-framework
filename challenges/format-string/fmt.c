8e7af1ae66db6e76a440615d2355feb1888d9e49
ahuva12 <ahuvaeshit@gmail.com>
2026-05-03 17:39:02 +0300

    challenges

--- challenges/format-string/fmt.c
+++ challenges/format-string/fmt.c
@@ -13,8 +13,7 @@ int main() {
     char input[100];
 	void (*func_ptr)() = safe;	
     void *addr_func_ptr = &func_ptr;
-//  printf("Hint: %p\n", &func_ptr);
-//  puts("Something in your input might be interpreted, not printed.");
+
     printf("What is your name?\n");
 	
     fgets(input, sizeof(input), stdin);
